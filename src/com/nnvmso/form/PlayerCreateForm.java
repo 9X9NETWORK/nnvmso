5a1f2c42db41f7421441684b44bfe95b90dfc269
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2010-11-11 22:57:22 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@58 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ src/com/nnvmso/form/PlayerCreateForm.java
@@ -0,0 +1,23 @@
+package com.nnvmso.form;
+
+import com.nnvmso.model.Mso;
+import com.nnvmso.model.Player;
+
+public class PlayerCreateForm {
+	private Mso mso;
+	private Player player;
+	
+	public Mso getMso() {
+		return mso;
+	}
+	public void setMso(Mso mso) {
+		this.mso = mso;
+	}
+	public Player getPlayer() {
+		return player;
+	}
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+	
+}
---
+++ src/com/nnvmso/form/UserSignupForm.java
@@ -0,0 +1,24 @@
+package com.nnvmso.form;
+
+import com.nnvmso.model.Mso;
+import com.nnvmso.model.NnUser;
+
+public class UserSignupForm {
+	private NnUser user;
+	private Mso mso;
+		
+	public NnUser getUser() {
+		return user;
+	}
+	public void setUser(NnUser user) {
+		this.user = user;
+	}
+	public Mso getMso() {
+		return mso;
+	}
+	public void setMso(Mso mso) {
+		this.mso = mso;
+	}
+	
+	
+}
