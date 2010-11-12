07dbaf7d8f9e0d7f9946373dcc559152b0b05f32
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2010-11-12 23:03:21 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@73 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ src/com/nnvmso/lib/CookieHelper.java
@@ -0,0 +1,24 @@
+package com.nnvmso.lib;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CookieHelper {
+
+	//add expiration
+	static public void setCookie(HttpServletResponse resp, String cookieName, String cookieValue) {		
+		Cookie cookie = new Cookie(cookieName, cookieValue);
+		resp.addCookie(cookie);		
+	}
+	
+	static public String readCookie(HttpServletRequest req, String cookieName) {
+		Cookie[] cookies = req.getCookies();
+		for (int i = 0; i < cookies.length; i++) {
+			if (cookies[i].getName().equals(cookieName)) {
+				return cookies[i].getValue();
+			}           
+		}
+		return null;
+	}
+}
