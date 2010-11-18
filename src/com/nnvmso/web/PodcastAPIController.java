4d52afc105ce742c444cd9cbad2e0311df09b30b
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2010-11-18 01:15:25 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@102 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/web/PodcastAPIController.java
+++ src/com/nnvmso/web/PodcastAPIController.java
@@ -1,9 +1,14 @@
 package com.nnvmso.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.nnvmso.json.PodcastChannel;
 import com.nnvmso.json.PodcastKeys;
@@ -16,6 +21,14 @@ import com.nnvmso.service.ChannelManager;
 import com.nnvmso.service.MsoManager;
 import com.nnvmso.service.ProgramManager;
 
+/**
+ * <p>Serves for Transcoding Service.</p>
+ * <p>Url examples: (notice method name is used at the end of URL) <br/> 
+ * http://hostname:port/podcastAPI/itemUpdate<br/>
+ * http://hostname:port/podcastAPI/channelUpdate<br/>
+ * <p/>
+ *
+ */
 @Controller
 @RequestMapping("podcastAPI")
 public class PodcastAPIController {
@@ -32,31 +45,57 @@ public class PodcastAPIController {
 		this.msoMngr = msoMngr;
 	}
 	
+	/**
+	 * Transcoding Service update Podcast Program information
+	 * 
+	 * @param podcastProgram podcastProgram returns in Json format
+	 * @return keys keys include channel key and item key
+	 */
+	
 	@RequestMapping("itemUpdate")
-	public String itemUpdate(@RequestBody PodcastProgram podcastProgram) {
+	public @ResponseBody PodcastKeys itemUpdate(@RequestBody PodcastProgram podcastProgram) {
+		PodcastKeys keys = new PodcastKeys();
 		if (podcastProgram.getAction().equals(PodcastProgram.ACTION_UPDATE_ITEM)) {
 			System.out.println("update item:" + podcastProgram.getKey());
-			PodcastKeys keys = new PodcastKeys();
 			MsoProgram p = programMngr.createViaPodcast(podcastProgram);
 			keys.setKey(podcastProgram.getKey());
-			keys.setItemKey(NnLib.getKeyStr(p.getKey()));
-			NnLib.urlFetch(TRANSCODING_SERVER, keys);
+			keys.setItemKey(NnLib.getKeyStr(p.getKey()));			
 		} else {
 			System.out.println("update enclosure:" + podcastProgram.getKey() + ";" + podcastProgram.getItem().getItemKey());
 			programMngr.saveViaPodcast(podcastProgram);
 		}
-		System.out.println("back to itemUpdate");
-		return "";
+		System.out.println("Finish itemUpdate");
+		return keys;
 	}
-		
+	
+	/**
+	 * Transcoding service update Podcast Channel Information
+	 * 
+	 * @param podcast podcast in json type <br/>
+	 * {  <br/>
+	 *    "action":"updateChannel", <br/>
+	 *    "key":"channel_key_id", <br/>
+	 *    "title":"channel_title", <br/>
+	 *    "description":"channel_description", <br/>    
+	 *    "pubDate":"channel_pubDate", <br/>
+	 *    "image":"channel_thumbnail",<br/>   
+	 *    "errorCode":0, <br/>
+	 *    "errorReason":"error description" <br/>     
+	 * } 
+	 */
 	@RequestMapping("channelUpdate")
-	public void channelUpdate(@RequestBody PodcastChannel podcast) {
+	public ResponseEntity<String> channelUpdate(@RequestBody PodcastChannel podcast) {
 		System.out.println("update:" + podcast.getKey());
 		MsoChannel channel = channelMngr.findByKey(podcast.getKey());
 		channelMngr.saveViaPodcast(channel, podcast);
-		System.out.println("back to channelupdate");
+		System.out.println("finish channelUpdate");
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.TEXT_PLAIN);
+		return new ResponseEntity<String>("OK", headers, HttpStatus.OK);
+		
 	}
 
+	/*
 	//dysfunction
 	@RequestMapping("batch_create")
 	public String batchCreate(@RequestBody PodcastChannel podcast) {
@@ -65,6 +104,6 @@ public class PodcastAPIController {
 		//programMngr.saveAllViaPodcast(podcast.getItems(), channel);		
 		return "";
 	}
-	
+	*/
 
 }
