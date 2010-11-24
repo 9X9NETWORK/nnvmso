6ee76492e65895d00c5a134843e142b1370ba4c1
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2010-11-24 02:21:33 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@127 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/lib/DebugLib.java
+++ src/com/nnvmso/lib/DebugLib.java
@@ -2,8 +2,7 @@ package com.nnvmso.lib;
 
 public class DebugLib {
 
-	public static final boolean NNDEBUG = true;
 	public static final String OUT = "----- DEBUG:";
-	public static final boolean NNDEVEL = true;
-	public static final boolean NNAWS = true;
+	public static final boolean NNDEVEL = true;	//if want to hack under devel mode
+	public static final boolean LOCAL_TEST = true; //for those need integration with other components, it is used when testing is done locally.
 }
