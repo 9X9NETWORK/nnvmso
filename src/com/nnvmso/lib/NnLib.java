adba1e40a24209cf865ab84709e626381c40a3f8
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2010-12-16 01:26:48 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@218 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/lib/NnLib.java
+++ src/com/nnvmso/lib/NnLib.java
@@ -3,6 +3,10 @@ package com.nnvmso.lib;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Date;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -10,21 +14,36 @@ import org.codehaus.jackson.map.ObjectMapper;
 
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.nnvmso.web.TestController;
 
 public class NnLib {
+	
+	protected final static Logger logger = Logger.getLogger(TestController.class.getName());
+	
 	public static String getKeyStr(Key key) {
 		return KeyFactory.keyToString(key);
 	}
 
-	public static String getUrlRoot(HttpServletRequest req) {	
-		String host = req.getLocalAddr();
-		String port = Integer.toString(req.getLocalPort());
-		if (port.equals("80")) {
-			port = "";
-		} else {
-			port = ":" + port;
+	public static String getUrlRoot(HttpServletRequest req) {
+		String url = req.getRequestURL().toString();
+	    Pattern p = Pattern.compile("(^http://.*?)/(.*)");	    	    
+	    Matcher m = p.matcher(url);
+	    String host = "";
+	    if (m.find()) {
+	    	host = m.group(1);
+	    }
+		return host; 		
+	}
+	
+	public static void logException(Exception e) {
+		String detail = "";
+		StackTraceElement[] elements = e.getStackTrace();
+		for (StackTraceElement elm:elements ) {
+			detail = detail + elm.toString() + "\n";			
 		}
-		return "http://" + host + port; 		
+		logger.severe("exception:" + e.toString());
+		logger.severe("exception stacktrace:\n" + detail);
+		String now = (new Date()).toString();		
 	}
 	
 	public static void urlPostWithJson(String urlStr, Object obj) {
