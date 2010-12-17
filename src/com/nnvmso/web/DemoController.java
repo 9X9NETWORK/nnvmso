fc55977229f64cc9ace73b82f7a1f0679843c274
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2010-12-17 22:39:08 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@248 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ src/com/nnvmso/web/DemoController.java
@@ -0,0 +1,128 @@
+package com.nnvmso.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.nnvmso.lib.NnLib;
+import com.nnvmso.lib.PlayerLib;
+import com.nnvmso.model.MsoChannel;
+import com.nnvmso.model.MsoProgram;
+import com.nnvmso.service.ChannelManager;
+import com.nnvmso.service.ProgramManager;
+
+@Controller
+@RequestMapping("demo")
+public class DemoController {
+	
+	private final ProgramManager programMngr; 
+	private final ChannelManager channelMngr;
+	
+	@Autowired
+	public DemoController(ProgramManager programMngr, ChannelManager channelMngr) {
+		this.programMngr = programMngr;
+		this.channelMngr = channelMngr;
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public String exception(Exception e) {
+		NnLib.logException(e);
+		return "error/exception";				
+	}		
+	
+	@RequestMapping("programList")
+	public ResponseEntity<String> programList(@RequestParam(value="channelKey", required = false)String channelKey) {
+		List<MsoProgram> programs = new ArrayList<MsoProgram>();		
+		if (channelKey == null) {	
+			programs = programMngr.findAll(); 
+		} else {
+			MsoChannel c = channelMngr.findByKey(channelKey);
+			if (c != null) {
+				programs = programMngr.findByChannel(c);
+			} else {
+				return PlayerLib.outputReturn("Channel not found");
+			}
+		}
+		String output = "";
+		for (MsoProgram p : programs) {
+			String cKey = NnLib.getKeyStr(p.getChannelKey());
+			String flv = p.getOtherFileUrl();
+			String mpeg4 = p.getMpeg4FileUrl();			
+			String[] ori = {cKey, flv, mpeg4}; 
+			output = output + this.getDelimitedStr(ori);
+			output = output + "\n";
+		}		
+		
+		return PlayerLib.outputReturn(output);
+	}
+		
+	private String getDelimitedStr(String[] ori) {
+		String delimiter = "||";
+		StringBuilder result = new StringBuilder();
+		if (ori.length > 0) {
+			result.append(ori[0]);
+		    for (int i=1; i<ori.length; i++) {
+		       result.append(delimiter);
+		       result.append(ori[i]);
+		    }
+		}
+		return result.toString();
+	}
+		
+	@RequestMapping("replaceFileUrls")
+	public ResponseEntity<String> replaceWithSGFileUrl(
+			@RequestParam(value="host", required=false) String host, 
+			@RequestParam(value="sg", required=false) String sg, 
+			@RequestParam(value="channelKey", required = false)String channelKey) {
+		
+		List<MsoProgram> programs = new ArrayList<MsoProgram>();
+		if (channelKey == null) {	
+			programs = programMngr.findAll(); 
+		} else {
+			MsoChannel c = channelMngr.findByKey(channelKey);
+			if (c != null) {
+				programs = programMngr.findByChannel(c);
+			} else {
+				return PlayerLib.outputReturn("Channel not found");
+			}
+		}
+		String regex = "^http://.*?/";		
+		for (MsoProgram p : programs) {	
+			String newHost = "http://" + host + "/";
+			String flvFileUrl = p.getOtherFileUrl();
+			if (flvFileUrl != null) {
+				if (host!=null) {
+					flvFileUrl = flvFileUrl.replaceAll(regex, newHost);
+				}
+				if (sg != null && sg.equals("1")) {
+					flvFileUrl = flvFileUrl.replaceFirst("/9x9cache", "/sg9x9cache");
+					flvFileUrl = flvFileUrl.replaceFirst("/9x9pod", "/sg9x9pod");
+				}
+				p.setOtherFileUrl(flvFileUrl);
+			}
+			String mpeg4FileUrl = p.getMpeg4FileUrl(); 
+			if (mpeg4FileUrl != null){
+				if (host!=null) {
+					mpeg4FileUrl = mpeg4FileUrl.replaceAll(regex, newHost);
+				}
+				if (sg != null && sg.equals("1")) {
+					mpeg4FileUrl = mpeg4FileUrl.replaceFirst("/9x9cache", "/sg9x9cache");
+					mpeg4FileUrl = mpeg4FileUrl.replaceFirst("/9x9cache", "/sg9x9pod");
+				}
+				p.setMpeg4FileUrl(mpeg4FileUrl);
+			}
+			
+			System.out.println(p.getOtherFileUrl() + ";" + p.getMpeg4FileUrl());
+			programMngr.save(p);
+		}
+		String output = "success, " + programs.size() + " program is modified.";		
+		return PlayerLib.outputReturn(output);
+	}
+	
+}
