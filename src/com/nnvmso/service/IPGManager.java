286a4c7018734dcaba1cc41d058d783f85f79eea
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2010-12-28 01:05:39 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@283 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/service/IPGManager.java
+++ src/com/nnvmso/service/IPGManager.java
@@ -3,6 +3,7 @@ package com.nnvmso.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.google.appengine.api.datastore.Key;
