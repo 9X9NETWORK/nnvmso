5a43d10087126cfdd11ee09bbd6506563f9603b4
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-01-26 21:47:31 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@388 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/lib/DebugLib.java
+++
@@ -1,7 +0,0 @@
-package com.nnvmso.lib;
-
-public class DebugLib {
-
-	public static final boolean NNDEVEL = true;	//if want to hack under devel mode
-	public static final boolean LOCAL_TEST = true; //for those need integration with other components, it is used when testing is done locally.
-}
--- src/com/nnvmso/lib/Prop.java
+++
@@ -1,6 +0,0 @@
-package com.nnvmso.lib;
-
-public class Prop {
-	
-	public static final String defaultMsoEmail = "default_mso@9x9.com";
-}
