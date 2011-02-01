04b95093aeb0c2398aed0f713a719e584c61190d
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-02-01 20:23:12 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@513 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ src/com/nnvmso/model/SubscriptionLog.java
@@ -0,0 +1,67 @@
+package com.nnvmso.model;
+
+import java.io.Serializable;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+
+@SuppressWarnings("serial")
+@PersistenceCapable(detachable="true")
+public class SubscriptionLog implements Serializable {
+	
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key key;
+	
+	@Persistent
+	private long msoId;
+	
+	@Persistent
+	private long channelId;
+	
+	@Persistent
+	private int count;
+
+	public SubscriptionLog(long msoId, long channelId) {
+		this.msoId = msoId;
+		this.channelId = channelId;
+		this.count = 1;
+	}
+	
+	public Key getKey() {
+		return key;
+	}
+
+	public void setKey(Key key) {
+		this.key = key;
+	}
+
+	public long getMsoId() {
+		return msoId;
+	}
+
+	public void setMsoId(long msoId) {
+		this.msoId = msoId;
+	}
+
+	public long getChannelId() {
+		return channelId;
+	}
+
+	public void setChannelId(long channelId) {
+		this.channelId = channelId;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+		
+}
