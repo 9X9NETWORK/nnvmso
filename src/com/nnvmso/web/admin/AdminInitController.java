da7a2241f5836d0ab896984bd43a17304525d516
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-02-01 20:25:50 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@517 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/web/admin/AdminInitController.java
+++ src/com/nnvmso/web/admin/AdminInitController.java
@@ -2,8 +2,10 @@ package com.nnvmso.web.admin;
 
 import java.util.logging.Logger;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,12 +18,19 @@ import com.nnvmso.lib.NnNetUtil;
 import com.nnvmso.service.InitService;
 
 /**
- * for testing, works only for small set of data
+ * for testing only, works only for small set of data
  */	
 @Controller
 @RequestMapping("admin/init")
 public class AdminInitController {
 	protected static final Logger logger = Logger.getLogger(AdminInitController.class.getName());		
+	
+	private final InitService initService;		
+	
+	@Autowired
+	public AdminInitController(InitService initService) {
+		this.initService = initService;
+	}		
 	
 	@ExceptionHandler(Exception.class)
 	public String exception(Exception e) {
@@ -30,30 +39,25 @@ public class AdminInitController {
 	}
 
 	@RequestMapping("deleteAll")
-	public ResponseEntity<String> initialize() {
-		InitService initService = new InitService();
+	public ResponseEntity<String> initialize(HttpServletRequest req) {
+		initService.setRequest(req);
 		initService.deleteAll();
 		return NnNetUtil.textReturn("Done.\nYou might also want to use \"initMsoAndCategories\"?");
 	}
-
+ 
 	@RequestMapping("initMsoAndCategories")
-	public ResponseEntity<String> initCategories() {
-		InitService initService = new InitService();
-		initService.initMsoAndCategories();
+	public ResponseEntity<String> initCategories(HttpServletRequest req, @RequestParam(value="debug")boolean debug) {
+		initService.setRequest(req);
+		initService.initMsoAndCategories(debug);
 		return NnNetUtil.textReturn("OK");		
 	}
 		
-	@RequestMapping("initAllDevel")
-	public ResponseEntity<String> initAll() {
-		InitService initService = new InitService();
-		initService.initAll(true);
-		return NnNetUtil.textReturn("OK");		
-	}
-
-	@RequestMapping("initAllIntegration")
-	public ResponseEntity<String> initAllIntegration() {
-		InitService initService = new InitService();
-		initService.initAll(false);
+	//devel mode, whether not to submit data to transcoding service
+	//debug mode, whether to turn on player's debugging information
+	@RequestMapping("initAll")
+	public ResponseEntity<String> initAll(@RequestParam(value="devel")boolean devel, @RequestParam(value="debug")boolean debug, HttpServletRequest req) { 
+		initService.setRequest(req);
+		initService.initAll(devel, debug);
 		return NnNetUtil.textReturn("OK");		
 	}
 	
