6d5691fbab8554467d05abc334c735ebc0030fec
william <william@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-02-02 21:59:03 +0000

    1st working version
    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@552 a0a5f996-2bb4-401c-8778-395c3c01700f

--- 9x9setenv.bat
+++ 9x9setenv.bat
@@ -1,5 +1,5 @@
 set ANT_HOME=C:\Bin\apache-ant-1.8.2
 set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_23
 set CLASSPATH=
-set PATH=%PATH%;%ANT_HOME%\bin
+set PATH=..\lib\appengine-java-sdk-1.4.0\bin;%JAVA_HOME%\bin;%ANT_HOME%\bin;%PATH%;
 
