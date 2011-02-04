c635e4eca5e3f794a9c9cb653634f7a5c9f43636
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-02-04 02:54:52 +0000

    add comment on create
    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@583 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/service/MsoIpgManager.java
+++ src/com/nnvmso/service/MsoIpgManager.java
@@ -4,14 +4,22 @@ import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.springframework.stereotype.Service;
+
 import com.nnvmso.dao.MsoIpgDao;
 import com.nnvmso.model.MsoIpg;
 
+@Service
 public class MsoIpgManager {
 	protected static final Logger log = Logger.getLogger(MsoIpgManager.class.getName());
 	
 	private MsoIpgDao msoIpgDao = new MsoIpgDao();
  
+	/**
+	 * @@@ IMPORTANT: 
+	 * Check channel status is your responsibility (for now, for quick initialization work).
+	 * Please use MsoChannelManager, isCounterQualified(MsoChannel channel) 
+	 */
 	public void create(MsoIpg msoIpg) {
 		Date now = new Date();
 		msoIpg.setCreateDate(now);
