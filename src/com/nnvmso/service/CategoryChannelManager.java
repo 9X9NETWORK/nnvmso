49eb318ec6a8657b7fee2a82aace89e9d3988fa0
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-02-08 03:13:54 +0000

    add delete & findByCategoryIdAndChanelId
    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@643 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/service/CategoryChannelManager.java
+++ src/com/nnvmso/service/CategoryChannelManager.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import com.nnvmso.dao.CategoryChannelDao;
+import com.nnvmso.model.Category;
 import com.nnvmso.model.CategoryChannel;
 
 public class CategoryChannelManager {
@@ -17,6 +18,14 @@ public class CategoryChannelManager {
 		ccDao.save(cc);
 	}	
 
+	public void delete(CategoryChannel cc) {
+		CategoryManager categoryMngr = new CategoryManager();
+		Category category = categoryMngr.findById(cc.getCategoryId()); 
+		ccDao.delete(cc);
+		category.setChannelCount(category.getChannelCount()-1);
+		categoryMngr.save(category);
+	}
+	
 	public List<CategoryChannel> findAllByCategoryId(long categoryId) {
 		List<CategoryChannel> ccs = ccDao.findAllByCategoryId(categoryId);
 		System.out.println("findByCategoryKey(): found " + ccs.size() + " with id " + categoryId);
@@ -26,6 +35,10 @@ public class CategoryChannelManager {
 	public List<CategoryChannel> findAllByChannelId(long channelId) {
 		List<CategoryChannel> ccs = ccDao.findAllByChannelId(channelId);
 		return ccs;
+	}
+
+	public CategoryChannel findByCategoryIdAndChanelId(long categoryId, long channelId) {
+		return ccDao.findByCategoryIdAndChannelId(categoryId, channelId);
 	}
 	
 	public CategoryChannel findById(long id) {
