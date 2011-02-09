ac8fcd737f6091debcef0a2f06197570aa51bdff
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-02-09 22:24:13 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@690 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/web/admin/AdminInitController.java
+++ src/com/nnvmso/web/admin/AdminInitController.java
@@ -4,13 +4,10 @@ import java.util.Properties;
 import java.util.logging.Logger;
 
 import javax.mail.Message;
-import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -83,13 +80,13 @@ public class AdminInitController {
 	private void sendEmail(String subject) {
 		Properties props = new Properties();
         Session session = Session.getDefaultInstance(props, null);
-
+        String msgBody = "done";
         try {
         	Message msg = new MimeMessage(session);
             msg.setFrom(new InternetAddress("nncloudtv@gmail.com", "nncloudtv"));
             msg.addRecipient(Message.RecipientType.TO, new InternetAddress("nncloudtv@gmail.com", "nncloudtv"));                             
             msg.setSubject(subject);
-            msg.setText("");
+            msg.setText(msgBody);
             Transport.send(msg);
         } catch (Exception e) {
         	NnLogUtil.logException(e);
