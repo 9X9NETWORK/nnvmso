b2344821e0877399cdb4bf7782667419a884bf64
louis <louis@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-02-14 06:12:49 +0000

    Remove dynamic java code from zooatomic.jsp
    This should improves loading time.
    
    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@760 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/web/PlayerController.java
+++ src/com/nnvmso/web/PlayerController.java
@@ -1,6 +1,8 @@
 package com.nnvmso.web;
 
+import java.util.Date;
 import java.util.logging.Logger;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 import com.nnvmso.lib.*;
 
@@ -24,10 +27,12 @@ public class PlayerController {
 	}		
 	
 	@RequestMapping("zooatomics")
-	public String zooatomics(@RequestParam(value="mso",required=false) String mso, HttpServletRequest req, HttpServletResponse resp) {		
+	public String zooatomics(@RequestParam(value="mso",required=false) String mso, HttpServletRequest req, HttpServletResponse resp, Model model) {		
 		if (CookieHelper.getCookie(req, CookieHelper.PLATFORM) == null) {
 			CookieHelper.setCookie(resp, CookieHelper.PLATFORM, CookieHelper.PLATFORM_GAE);
 		}		
+		String now = (new SimpleDateFormat("MM.dd.yyyy")).format(new Date()).toString();
+		model.addAttribute("now", now);
 		CookieHelper.setCookie(resp, CookieHelper.MSO, mso);
 		return "player/zooatomics";
 	}
--- src/com/nnvmso/web/ShareController.java
+++ src/com/nnvmso/web/ShareController.java
@@ -1,8 +1,10 @@
 package com.nnvmso.web;
 
+import java.util.Date;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import java.lang.Long;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 
 import com.nnvmso.lib.*;
 import com.nnvmso.model.*;
@@ -29,7 +32,7 @@ public class ShareController {
 	}
 	
 	@RequestMapping("{ipgId}")
-	public String zooatomics(@PathVariable String ipgId, HttpServletRequest req, HttpServletResponse resp) {
+	public String zooatomics(@PathVariable String ipgId, HttpServletRequest req, HttpServletResponse resp, Model model) {
 		log.info("/share/" + ipgId);
 		IpgManager ipgMngr = new IpgManager();
 		if (!Pattern.matches("^\\d*$", ipgId)) {
@@ -40,6 +43,8 @@ public class ShareController {
 		if (ipg == null) {
 			return "redirect:/";
 		}
+		String now = (new SimpleDateFormat("MM.dd.yyyy")).format(new Date()).toString();
+		model.addAttribute("now", now);
 		return "player/zooatomics";
 	}
 }
