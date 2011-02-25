73d29d38252c90650bde8e83120d96aa50a2b68e
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-02-25 02:23:01 +0000

    include data processing
    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@883 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/task/mapper/MigrateUpMsoChannelMapper.java
+++ src/com/nnvmso/task/mapper/MigrateUpMsoChannelMapper.java
@@ -6,6 +6,7 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.tools.mapreduce.AppEngineMapper;
 import com.google.appengine.tools.mapreduce.DatastoreMutationPool;
+import com.nnvmso.model.MsoChannel;
 
 /**
  * one time deal, add new fields 
@@ -17,9 +18,18 @@ public class MigrateUpMsoChannelMapper extends AppEngineMapper<Key, Entity, Null
 	public void map(Key key, Entity value, Context context) {
 		String name = (String)value.getProperty("name");
 		String sourceUrl = (String)value.getProperty("sourceUrl");
-
-		if (sourceUrl != null) {sourceUrl = sourceUrl.toLowerCase();} 
+		short type = MsoChannel.CONTENTTYPE_PODCAST;
+		if (sourceUrl != null) {
+			sourceUrl = sourceUrl.toLowerCase();
+			if (sourceUrl.contains("http://www.youtube.com") || sourceUrl.contains("https://www.youtube.com") || 
+					sourceUrl.contains("http://youtube.com") || sourceUrl.contains("https://youtube.com")) {			
+				type = MsoChannel.CONTENTTYPE_YOUTUBE;
+			} else if (sourceUrl.equals("http://9x9pod.s3.amazonaws.com/default.mp4")) {
+				type = MsoChannel.CONTENTTYPE_SYSTEM;
+			}
+		} 
 		if (name != null) {name = name.toLowerCase();}
+		value.setProperty("contentType", type);
 		value.setProperty("sourceUrlSearch", sourceUrl);
 		value.setProperty("nameSearch", name);
 		value.setProperty("enforceTranscoding", 0);
