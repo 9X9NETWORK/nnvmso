745cb19cdf2b21704aa86e34ee637c4cea6d5d75
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-03-09 23:31:37 +0000

    support properties
    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@1002 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/task/mapper/AddPropertyMapper.java
+++ src/com/nnvmso/task/mapper/AddPropertyMapper.java
@@ -1,5 +1,6 @@
 package com.nnvmso.task.mapper;
 
+import java.util.Date;
 import java.util.logging.Logger;
 
 import org.apache.hadoop.io.NullWritable;
@@ -13,11 +14,22 @@ public class AddPropertyMapper extends AppEngineMapper<Key, Entity, NullWritable
 	private static final Logger log = Logger.getLogger(AddPropertyMapper.class.getName());
 
 	@Override
-	public void map(Key key, Entity value, Context context) {
-		log.info("Adding key to deletion pool: " + key);
+	public void map(Key key, Entity value, Context context) {		
 		String propertyName = context.getConfiguration().get("propertyName");
 		String propertyValue = context.getConfiguration().get("propertyValue");
-		value.setProperty(propertyName, propertyValue);
+		String type = context.getConfiguration().get("propertyType");
+		
+		log.info("Adding property -- property name=" + propertyName + ";value=" + propertyValue + ";type=" + type);
+		
+		if (type.equals("int")) {
+			value.setProperty(propertyName, Integer.parseInt(propertyValue));			
+		} else if (type.equals("boolean")) {
+			value.setProperty(propertyName, Boolean.parseBoolean(propertyValue));
+		} else if (type.equals("date")) {
+			value.setProperty(propertyName, new Date());
+		} else if (type.equals("string")) {
+			value.setProperty(propertyName, propertyValue);
+		}
 		DatastoreMutationPool mutationPool = this.getAppEngineContext(context).getMutationPool();
 		mutationPool.put(value);
 	}
