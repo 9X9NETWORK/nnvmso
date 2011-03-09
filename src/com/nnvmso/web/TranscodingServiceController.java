ace6b08f3a1af7edf8e39e6a8e82eaf4ac9db85a
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-03-09 23:37:16 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@1007 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/web/TranscodingServiceController.java
+++ src/com/nnvmso/web/TranscodingServiceController.java
@@ -1,27 +1,15 @@
 package com.nnvmso.web;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.codec.binary.Base64;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.SerializationConfig.Feature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
