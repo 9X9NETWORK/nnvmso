10376fb79d4182fe548992159247fc2c33f16667
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-03-25 00:53:29 +0000

    add channelCount fix and remove programs > 50
    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@1099 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/web/task/CalibrationTask.java
+++ src/com/nnvmso/web/task/CalibrationTask.java
@@ -16,9 +16,11 @@ import com.nnvmso.lib.NnNetUtil;
 import com.nnvmso.model.Category;
 import com.nnvmso.model.CategoryChannel;
 import com.nnvmso.model.MsoChannel;
+import com.nnvmso.model.MsoProgram;
 import com.nnvmso.service.CategoryChannelManager;
 import com.nnvmso.service.CategoryManager;
 import com.nnvmso.service.MsoChannelManager;
+import com.nnvmso.service.MsoProgramManager;
 
 /**
  * temporary fix, to be removed
@@ -29,6 +31,7 @@ import com.nnvmso.service.MsoChannelManager;
 public class CalibrationTask {
 	protected static final Logger log = Logger.getLogger(CalibrationTask.class.getName());
 	
+	//entry, entry, correct category's channelCount, categoryChannelCount -> runCategories -> runCategoryChannels
 	@RequestMapping("categoryChannelCount")
 	public ResponseEntity<String> categoryChannelCount() throws IOException {
 		try {						
@@ -42,6 +45,7 @@ public class CalibrationTask {
 		return NnNetUtil.textReturn("OK");
 	}
 
+	//run through each category, to envoke categorychannel count
 	@RequestMapping(value="runCategories")
 	public ResponseEntity<String> runCategories(HttpServletRequest req) {
 		String output = "";
@@ -60,6 +64,7 @@ public class CalibrationTask {
 		return NnNetUtil.textReturn(output);
 	}
 	
+	//update categorychannel count
 	@RequestMapping(value="runCategoryChannels")
 	public ResponseEntity<String> runCategoryChannels(HttpServletRequest req) {
 		int categoryId = Integer.parseInt(req.getParameter("category"));
@@ -72,5 +77,102 @@ public class CalibrationTask {
 		log.info("calibrat category:" + category.getName() + "(" + category.getKey().getId() + ") with " + list.size() + " channels.");
 		return NnNetUtil.textReturn("OK");
 	}	
+
+	//entry, correct channel's programCount: channelCount -> runChannels -> runPrograms
+	@RequestMapping("channelCount")
+	public ResponseEntity<String> channelCount() throws IOException {
+		try {						
+			QueueFactory.getDefaultQueue().add(
+					TaskOptions.Builder.withUrl("/task/calibration/runChannels")
+		    );
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return NnNetUtil.textReturn("OK");
+	}
+
+	//run through each channel, 
+	@RequestMapping(value="runChannels")
+	public ResponseEntity<String> runChannels(HttpServletRequest req) {
+		String output = "";
+		MsoChannelManager channelMngr = new MsoChannelManager();
+		List<MsoChannel> channels = channelMngr.findAll();
+		for (MsoChannel c : channels) {			
+			try {						
+				QueueFactory.getDefaultQueue().add(
+						TaskOptions.Builder.withUrl("/task/calibration/runPrograms")
+						     .param("channel", String.valueOf(c.getKey().getId()))
+			    );
+			} catch (Exception e) {
+				e.printStackTrace();
+			}			
+		}
+		return NnNetUtil.textReturn(output);
+	}
+
+	@RequestMapping(value="runPrograms")
+	public ResponseEntity<String> runPrograms(HttpServletRequest req) {
+		int channelId = Integer.parseInt(req.getParameter("channel"));
+		String output = "";
+		MsoProgramManager programMngr = new MsoProgramManager();
+		List<MsoProgram> list = programMngr.findAllByChannelId(channelId);
+		int count = list.size();
+		log.info("program count: " + count + "(channelId:" + channelId + ")");
+		MsoChannelManager channelMngr = new MsoChannelManager();
+		MsoChannel channel = channelMngr.findById(channelId);
+		channel.setProgramCount(count);
+		channelMngr.save(channel);
+		return NnNetUtil.textReturn(output);
+	}
+
+	//entry, remove programs more than 50, programsRemoveEntry->programRemoveChannels->programsRemove
+	@RequestMapping("programsRemoveEntry")
+	public ResponseEntity<String> programsRemoveEntry() throws IOException {
+		try {						
+			QueueFactory.getDefaultQueue().add(
+					TaskOptions.Builder.withUrl("/task/calibration/programRemoveChannels")
+		    );
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return NnNetUtil.textReturn("OK");
+	}
+
+	//run through each channel, 
+	@RequestMapping(value="programRemoveChannels")
+	public ResponseEntity<String> programRemoveChannels(HttpServletRequest req) {
+		String output = "";
+		MsoChannelManager channelMngr = new MsoChannelManager();
+		List<MsoChannel> channels = channelMngr.findProgramsMoreThanMax();
+		log.info("# of channels > 50 episodes:" + channels.size());
+		for (MsoChannel c : channels) {			
+			try {						
+				QueueFactory.getDefaultQueue().add(
+						TaskOptions.Builder.withUrl("/task/calibration/programsRemove")
+						     .param("channel", String.valueOf(c.getKey().getId()))
+			    );
+			} catch (Exception e) {
+				e.printStackTrace();
+			}			
+		}
+		return NnNetUtil.textReturn(output);
+	}
+
+	@RequestMapping(value="programsRemove")
+	public ResponseEntity<String> programsRemove(HttpServletRequest req) {
+		int channelId = Integer.parseInt(req.getParameter("channel"));
+		String output = "";
+		MsoProgramManager programMngr = new MsoProgramManager();
+		int count = programMngr.findAndDeleteProgramsOlderThanMax(channelId);
+		log.info("program count: " + count + "(channelId:" + channelId + ")");
+		MsoChannelManager channelMngr = new MsoChannelManager();
+		MsoChannel channel = channelMngr.findById(channelId);
+		channel.setProgramCount(count);
+		channelMngr.save(channel);
+		return NnNetUtil.textReturn(output);
+	}
+	
 	
 }
