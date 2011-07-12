8b238c47a8742cf957551202c2a8ddc8e31f0762
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-07-12 02:06:56 +0000

    add slide process
    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso-cms@1659 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/web/ContentWorkerController.java
+++ src/com/nnvmso/web/ContentWorkerController.java
@@ -12,14 +12,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.nnvmso.lib.NnLogUtil;
+import com.nnvmso.model.MsoChannel;
+import com.nnvmso.model.MsoProgram;
 import com.nnvmso.service.ContentWorkerService;
+import com.nnvmso.service.MsoChannelManager;
+import com.nnvmso.service.MsoProgramManager;
+import com.nnvmso.service.NnStatusCode;
 import com.nnvmso.web.json.transcodingservice.ContentWorker;
 import com.nnvmso.web.json.transcodingservice.PostResponse;
+import com.nnvmso.web.json.transcodingservice.Slide;
 
 @Controller
 @RequestMapping("content_worker")
 public class ContentWorkerController {
-	protected static final Logger log = Logger.getLogger(TranscodingServiceController.class.getName());
+	protected static final Logger log = Logger.getLogger(ContentWorkerController.class.getName());
 	
 	private ContentWorkerService workerService;
 	
@@ -32,6 +38,32 @@ public class ContentWorkerController {
 	public String exception(Exception e) {
 		NnLogUtil.logException(e);
 		return "error/blank";
+	}
+	
+	@RequestMapping("saveNewSlide")
+	public @ResponseBody Slide saveNewSlide(@RequestBody Slide slide,
+            							    HttpServletRequest req) {
+		log.info(slide.toString());
+		MsoProgramManager programMngr = new MsoProgramManager();
+		MsoChannelManager channelMngr = new MsoChannelManager();		
+		MsoChannel channel = channelMngr.findById(Long.valueOf(slide.getChannelId()));		
+		if (channel == null) {
+			slide.setErrorCode(NnStatusCode.CHANNEL_INVALID);
+			slide.setErrorReason("CHANNEL_INVALID");
+			return slide;
+		}		
+		if (channel.getProgramCount() >= MsoChannelManager.MAX_CHANNEL_SIZE) {
+			MsoProgram oldest = programMngr.findOldestByChannelId(channel.getKey().getId());
+			programMngr.delete(oldest); 			
+		}
+		MsoProgram program = new MsoProgram(slide.getName(), slide.getIntro(), slide.getImageUrl(), MsoProgram.TYPE_SLIDE); 
+		program.setOtherFileUrl(slide.getFileUrl());
+		program.setDuration(slide.getDuration());
+		programMngr.create(channel, program);
+		slide.setProgramId(program.getKey().getId());
+		slide.setErrorCode(NnStatusCode.SUCCESS);
+		slide.setErrorReason("SUCCESS");
+		return slide;
 	}
 	
 	@RequestMapping("channel_logo_update")
