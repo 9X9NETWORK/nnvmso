9118cfe43ecdb6efda639fd8f1878a539bec2643
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-08-02 00:42:34 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@1804 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/service/CategoryManager.java
+++ src/com/nnvmso/service/CategoryManager.java
@@ -35,20 +35,14 @@ public class CategoryManager {
 			category.setCreateDate(now);
 			category.setUpdateDate(now);
 			categoryDao.save(category);
-			//save to cache
-			Cache cache = CacheFactory.get();
-			if (cache != null) { 
-				String key = this.getCacheKey(category.getMsoId(),category.getKey().getId());
-				cache.put(key, category);
-				String cntKey = this.getCacheCntKey(category.getMsoId());
-				@SuppressWarnings("unchecked")
-				List<Long> list = (List<Long>)cache.get(cntKey);
-				if (list == null) {list = new ArrayList<Long>();}
-				list.add(category.getKey().getId());
-				cache.put(cntKey, list);
-			}			
+			this.cacheRefresh(category);
 		}
 	}
+	
+	public void cacheRefresh(Category category) {
+		this.findAllByMsoId(category.getMsoId());
+	}
+	
 	public String translate(String name) {
 		if (name == null) return null;
 		Locale locale = Locale.TRADITIONAL_CHINESE;
@@ -90,10 +84,7 @@ public class CategoryManager {
 	public Category save(Category category) {
 		category.setUpdateDate(new Date());		
 		category = categoryDao.save(category);
-		//save to cache
-		Cache cache = CacheFactory.get();
-		String key = this.getCacheKey(category.getMsoId(),category.getKey().getId());
-		if (cache != null) { cache.put(key, category);	}				
+		this.cacheRefresh(category);
 		return category;
 	}
 	
@@ -196,46 +187,30 @@ public class CategoryManager {
 	
 	//result will be cached
 	public List<Category> findAllByMsoId(long msoId) {
-		List<Category> categories = new ArrayList<Category>();
 		Cache cache = CacheFactory.get();
-		List<Long> notCachedIds = new ArrayList<Long>();
-		//find from cache
+		String key = this.getCacheKey(msoId);
 		if (cache != null) {
 			@SuppressWarnings("unchecked")
-			List<Long> ids = (List<Long>)cache.get(this.getCacheCntKey(msoId));
-			if (ids != null) {
-				for (int i=0; i < ids.size(); i++) {
-					Category c = (Category)cache.get(this.getCacheKey(msoId, ids.get(i)));
-					if (c != null) { 
-						categories.add(c);
-					} else {
-						notCachedIds.add(ids.get(i));
-					}										
-				}
-				if (notCachedIds.size() ==0) {return categories;}				
+			List<Category> categories= (List<Category>) cache.get(key);
+			if (categories != null) { 
+				log.info("categories from cahce:" + categories.size());
+				return categories;
 			}
-		}
-		//find
-		if (notCachedIds.size() > 0) {
-			categories = this.findAllByIds(notCachedIds);
-			log.info("Categories are partial cached.");
-		} else {
-			categories = categoryDao.findAllByMsoId(msoId);
-			log.info("Categories never been cached");
-		}
-		//save in cache
-		if (cache != null) {
-			List<Long> ids = new ArrayList<Long>();
-			for (int i=0; i < categories.size(); i++) {
-				long id = categories.get(i).getKey().getId();
-				String key = this.getCacheKey(msoId, id);  
-				cache.put(key, categories.get(i));
-				ids.add(id);
-			}
-			String key = this.getCacheCntKey(msoId); 
-			cache.put(key, ids);
 		}		
-		return categories;
+		List<Category> categories = categoryDao.findAllByMsoId(msoId); //!!!hack
+		List<Category> sequence = new ArrayList<Category>();
+		Category others = null;
+		for (Category c : categories) {
+			if (!c.getName().equals("Others"))
+				sequence.add(c);
+			else
+				others = c;
+				
+		}
+		if (others != null)
+			sequence.add(others);
+		cache.put(key, sequence);
+		return sequence;
 	}
 	
 	public List<Category> findAll() {
@@ -253,51 +228,17 @@ public class CategoryManager {
 	public Category findById(long id) {
 		return categoryDao.findById(id);
 	}
-		
-	//example: mso(1)category(123), returns category
-	private String getCacheKey(long msoId, long categoryId) {
-		return "mso(" + msoId + ")category(" + categoryId + ")";
+			
+	private String getCacheKey(long msoId) {
+		return "mso(" + msoId + ")category(all)";
 	}
 	
-	//example: mso(1)categoryCnt, it returns a list of category id
-	private String getCacheCntKey(long msoId) {
-		return "mso(" + msoId + ")categoryCnt"; 		
-	}
-
 	public void deleteCache(long msoId) {
-		Cache cache = CacheFactory.get(); 
-		List<Category> categories = this.findAllByMsoId(msoId);
-		if (cache != null) {			
-			for (Category c : categories) {				
-				cache.remove(this.getCacheKey(msoId, c.getKey().getId()));
-			}
-			cache.remove(this.getCacheCntKey(msoId));
-		}
-	}		
-
-	public String findCache() {
-		String output = "";
-		String listOutput = "";
 		Cache cache = CacheFactory.get();
 		if (cache != null) {
-			List<Category> categories = this.findAll();
-			for (Category c : categories) {
-				Category category = (Category)cache.get(this.getCacheKey(c.getMsoId(), c.getKey().getId()));
-				 if (category != null) {
-					output = output + category.toString() + "\n";
-				}
-				@SuppressWarnings("unchecked")
-				List<Long> list = (List<Long>)cache.get(this.getCacheCntKey(c.getMsoId()));
-				if (list != null) {
-					listOutput = "\n category id under mso: " + c.getMsoId() + "\n";
-					for (Long l : list) {
-						listOutput = listOutput + l + "\t";
-					}
-				}
-			}
+			cache.remove(this.getCacheKey(msoId));
 		}
-		return output + listOutput;
-	}
+	}		
 
 	public List<Category> findAllByParentId(long parentId) {
 		return categoryDao.findAllByParanetId(parentId);
