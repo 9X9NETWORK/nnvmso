5bc59debce371d9168d2e70b198c745c75224baa
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-08-06 00:23:17 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso-cms@1839 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/task/mapper/MergeAccountMapper.java
+++ src/com/nnvmso/task/mapper/MergeAccountMapper.java
@@ -1,9 +1,10 @@
 package com.nnvmso.task.mapper;
 
-import java.util.logging.Logger;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.mapreduce.Counter;
 
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
@@ -12,44 +13,41 @@ import com.google.appengine.tools.mapreduce.DatastoreMutationPool;
 import com.nnvmso.model.NnUser;
 
 public class MergeAccountMapper extends AppEngineMapper<Key, Entity, NullWritable, NullWritable>{
-
-	protected static final Logger logger = Logger.getLogger(ReCategoryMapper.class.getName());
-		
+	
 	@Override
-	public void map(Key key, Entity entity, Context context) {
-		context.getCounter("AccountCount", "totalGuestCount").increment(1);
-
-		System.out.println(context.getCounter("AccountCount", "totalGuestCount").getValue());
-
-		/*
-		String toBeRemovedStr = context.getConfiguration().get("msoId");
-		long toBeRemovedId = Long.valueOf(toBeRemovedStr);
-				
-		DatastoreMutationPool mutationPool = this.getAppEngineContext(context).getMutationPool();		
-		String email= (String) entity.getProperty("email");
-		long msoId = (Long) entity.getProperty("msoId");
-		 */
-		
-		/*
-		if (email.equals("a@a.com")) {
-			System.out.println("<<<<<<<< email:" + email + ";msoId:" + msoId);
-		}
-		
-		//works only for 5f, 9x9, but not working for 9x9, 5f
-		if (!email.equals(NnUser.GUEST_EMAIL)) {
-			context.getCounter("user", email).increment(1);
-			Counter c = context.getCounter("user", email);
-			
-			System.out.println("email:" + email + ";counter=" + c.getValue());
-			if (c.getValue() > 1 && msoId == toBeRemovedId) {
-				System.out.println("entity removed:" + ";" + email);
-				mutationPool.delete(entity.getKey());
-			} else if (c.getValue() > 1 && msoId != toBeRemovedId) {
-				System.out.println("entity not able to removed:" + ";" + email);
+	public void map(Key key, Entity value, Context context) {			
+		String since = context.getConfiguration().get("since");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+		Date sinceDate = null;
+		try {
+			if (since!= null) {
+				sinceDate = sdf.parse(since);
 			}
+		} catch (ParseException e) {
+			return;
 		}
-		*/
+		
+		String email = (String) value.getProperty("email");
+		Date createDate = (Date)value.getProperty("createDate");
+		context.getCounter("AccountCount", "totalAccountCount").increment(1);
+		if (email != null) {
+			if (!email.equals(NnUser.GUEST_EMAIL)) {
+				//find 
+			}
+			if (email.equals(NnUser.GUEST_EMAIL)) {
+				context.getCounter("AccountCount", "totalGuestCount").increment(1);
+				if (createDate.after(sinceDate)) {
+					context.getCounter("AccountCount", "newGuestCount").increment(1);
+				}
+			} else {
+				context.getCounter("AccountCount", "totalUserCount").increment(1);
+				if (createDate.after(sinceDate)) {
+					context.getCounter("AccountCount", "newUserCount").increment(1);
+				}
+			}			
+		}
+		DatastoreMutationPool mutationPool = this.getAppEngineContext(context).getMutationPool();
+		mutationPool.put(value);
 	}
 	
 }
-	
