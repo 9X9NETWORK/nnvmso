07b0b9117ee9bcb245b474184534758d4817a391
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2011-08-18 23:34:33 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso-moveout@1913 a0a5f996-2bb4-401c-8778-395c3c01700f

--- nnqueue/src/com/nncloudtv/queue/Main.java
+++ nnqueue/src/com/nncloudtv/queue/Main.java
@@ -19,46 +19,55 @@ import com.rabbitmq.client.QueueingConsumer;
 public class Main
 {
 	
-    @SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	public static void main (String[] args) throws Exception {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        
-        Connection connection = factory.newConnection();
-        String name = args[0];
-        
-        //channel1
-        Channel channel = connection.createChannel();
-        channel.exchangeDeclare(name, "fanout");
-        String queueName = channel.queueDeclare().getQueue();
-        channel.queueBind(queueName, name, "");                        
-        QueueingConsumer consumer = new QueueingConsumer(channel);
-        channel.basicConsume(queueName, true, consumer);
-        
-        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-        MsoManager msoMngr = new MsoManager();
-        
-        while (true) {
-            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-	        String message = new String(delivery.getBody());
-	        if (name.equals(QueueMessage.BRAND_COUNTER)) {
-		        System.out.println(" [x] Brand Counter Received '" + message + "'");
-		        msoMngr.addMsoInfoVisitCounter(message);
-	        } else if (name.equals(QueueMessage.CATEGORY_CREATE)) {
-		        System.out.println(" [x] Category Create Received '" + message + "'");
-		        Object[] obj = (Object[])new QueueMessage().toObject(delivery.getBody());
-		        new CategoryManager().addCategory((Long)obj[0], (ArrayList<Category>)obj[1]);
-	        } else if (name.equals(QueueMessage.CHANNEL_CREATE_RELATED)) {
-	        	Object[] obj = (Object[])new QueueMessage().toObject(delivery.getBody());
-	        	NnChannel c = (NnChannel)obj[0];
-	        	List<Category> categories = (List<Category>)obj[1];
-	        	new CategoryManager().createChannelRelated(c, categories);
-            } else if (name.equals(QueueMessage.TRANSCODING_SUBMIT)) {
+		String exchange_name = args[0];
+		if (!exchange_name.equals(QueueMessage.HELLO) &&
+			!exchange_name.equals(QueueMessage.BRAND_COUNTER) && 
+			!exchange_name.equals(QueueMessage.CATEGORY_CREATE) && 
+			!exchange_name.equals(QueueMessage.CHANNEL_CREATE_RELATED) && 
+			!exchange_name.equals(QueueMessage.TRANSCODING_SUBMIT)) {		
+			System.out.println("[*] Please check your input [brand_counter, category_create, channel_create_related, transcoding_submit]");
+			System.exit(0);
+		}
+			
+		ConnectionFactory factory = new ConnectionFactory();
+	    //factory.setHost("localhost");
+	    //Connection connection = factory.newConnection();
+		Connection connection = factory.newConnection("localhost");
+	    Channel channel = connection.createChannel();                  
+	    channel.exchangeDeclare(exchange_name, "fanout");
+	    String queueName = channel.queueDeclare().getQueue();
+	    channel.queueBind(queueName, exchange_name, "");
+	    System.out.println(" [*] Waiting for messages from " + exchange_name + ". To exit press CTRL+C");
+	
+	    QueueingConsumer consumer = new QueueingConsumer(channel);
+	    channel.basicConsume(queueName, true, consumer);
+	     
+	    MsoManager msoMngr = new MsoManager();
+	
+	    while (true) {
+	       QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+	       String message = new String(delivery.getBody());
+	       System.out.println("message:" + message);
+	       if (exchange_name.equals(QueueMessage.BRAND_COUNTER)) {
+		       System.out.println(" [x] Brand Counter Received '" + message + "'");
+		       msoMngr.addMsoInfoVisitCounter(message);
+	       } else if (exchange_name.equals(QueueMessage.CATEGORY_CREATE)) {
+		       System.out.println(" [x] Category Create Received '" + message + "'");
+		       Object[] obj = (Object[])new QueueMessage().toObject(delivery.getBody());
+		       new CategoryManager().addCategory((Long)obj[0], (ArrayList<Category>)obj[1]);
+	       } else if (exchange_name.equals(QueueMessage.CHANNEL_CREATE_RELATED)) {
+	           Object[] obj = (Object[])new QueueMessage().toObject(delivery.getBody());
+	       	   NnChannel c = (NnChannel)obj[0];
+	           List<Category> categories = (List<Category>)obj[1];
+	           new CategoryManager().createChannelRelated(c, categories);
+	       } else if (exchange_name.equals(QueueMessage.TRANSCODING_SUBMIT)) {
 	        	/*
 				TranscodingService tranService = new TranscodingService();
-				tranService.submitToTranscodingService(channel.getId(), sourceUrl, req);	        	
+				tranService.submitToTranscodingService(channel.getId(), sourceUrl, req);
 				*/
-	        }
-        }
+	       }
+	    }
     }
 }
