d9c232d7a8528adc55ab5d732072c33a77f3079b
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-03-07 20:10:35 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso@2503 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/com/nnvmso/task/mapper/DMSubscriptionCounterMapper.java
+++ src/com/nnvmso/task/mapper/DMSubscriptionCounterMapper.java
@@ -1,5 +1,9 @@
 package com.nnvmso.task.mapper;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Counter;
 
@@ -12,19 +16,29 @@ public class DMSubscriptionCounterMapper extends AppEngineMapper<Key, Entity, Nu
 	
 	@Override
 	public void map(Key key, Entity value, Context context) {		
+		String before = context.getConfiguration().get("before");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+		Date beforeDate = null;
+		try {
+			beforeDate = sdf.parse(before);
+		} catch (ParseException e) {
+			return;
+		}
 		
-		long userId = (Long) value.getProperty("userId");
-		String userIdStr = String.valueOf(userId);
-		context.getCounter("SubCount", userIdStr).increment(1);
-		
-		Counter counter = context.getCounter("SubCount", userIdStr);
-		long cnt = counter.getValue();
-		long minus = cnt - 1;
-		context.getCounter("SubCount", String.valueOf(cnt)).increment(1);
-		context.getCounter("SubCount", String.valueOf(minus)).increment(-1);
-			
-		DatastoreMutationPool mutationPool = this.getAppEngineContext(context).getMutationPool();
-		mutationPool.put(value);
+		Date updateDate = (Date)value.getProperty("updateDate");
+		if (updateDate.before(beforeDate)) {		
+			long userId = (Long) value.getProperty("userId");
+			String userIdStr = String.valueOf(userId);
+			context.getCounter("SubCount", userIdStr).increment(1);					
+			Counter counter = context.getCounter("SubCount", userIdStr);			
+			long cnt = counter.getValue();
+			long minus = cnt - 1;
+			context.getCounter("SubCount", String.valueOf(cnt)).increment(1);
+			context.getCounter("SubCount", String.valueOf(minus)).increment(-1);
+				
+			DatastoreMutationPool mutationPool = this.getAppEngineContext(context).getMutationPool();
+			mutationPool.put(value);
+		}
 	}
 	
 }
