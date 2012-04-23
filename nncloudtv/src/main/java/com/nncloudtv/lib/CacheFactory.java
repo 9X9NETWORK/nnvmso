dee14698c9fa2957e1b3c1647deaaa1b885a5e79
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-04-23 19:58:32 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso-moveout@2643 a0a5f996-2bb4-401c-8778-395c3c01700f

--- nncloudtv/src/main/java/com/nncloudtv/lib/CacheFactory.java
+++ nncloudtv/src/main/java/com/nncloudtv/lib/CacheFactory.java
@@ -2,19 +2,28 @@ package com.nncloudtv.lib;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.logging.Logger;
 
 import net.spy.memcached.MemcachedClient;
 
 public class CacheFactory {
+	
+	protected static final Logger log = Logger.getLogger(CacheFactory.class.getName());
+	
 	private static MemcachedClient cache = null;
 	public static int EXP_DEFAULT = 2592000;
 	public static int PORT_DEFAULT = 11211;
 	
 	public static MemcachedClient get() {		
 		try {
-			cache = new MemcachedClient(new InetSocketAddress("localhost", CacheFactory.PORT_DEFAULT));				    
+			//TODO load it from memcache.properties, but make sure the property input stream is closed
+			cache = new MemcachedClient(new InetSocketAddress("localhost", CacheFactory.PORT_DEFAULT));		
 		} catch (IOException e) {	 
-		   e.printStackTrace();
+		   log.severe("memcache io exception");
+		   NnLogUtil.logException(e);
+		} catch (Exception e) {
+			log.severe("memcache exception");
+			NnLogUtil.logException(e);
 		}
 		
 		return cache;
