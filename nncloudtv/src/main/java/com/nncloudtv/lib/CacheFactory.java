e0b3511ffa281c0feddbb9d606b76c8ff9f8bfe3
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-04-27 23:48:40 +0000

    handle exception and shutdown in the lib
    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso-moveout@2669 a0a5f996-2bb4-401c-8778-395c3c01700f

--- nncloudtv/src/main/java/com/nncloudtv/lib/CacheFactory.java
+++ nncloudtv/src/main/java/com/nncloudtv/lib/CacheFactory.java
@@ -5,6 +5,7 @@ import java.net.InetSocketAddress;
 import java.util.logging.Logger;
 
 import net.spy.memcached.MemcachedClient;
+import net.spy.memcached.OperationTimeoutException;
 
 public class CacheFactory {
 	
@@ -13,20 +14,77 @@ public class CacheFactory {
 	private static MemcachedClient cache = null;
 	public static int EXP_DEFAULT = 2592000;
 	public static int PORT_DEFAULT = 11211;
+	public static String ERROR = "ERROR";
+	public static boolean isRunning = true;
 	
-	public static MemcachedClient get() {		
+	public static MemcachedClient getClient() {		
 		try {
 			//TODO load it from memcache.properties, but make sure the property input stream is closed
-			cache = new MemcachedClient(new InetSocketAddress("localhost", CacheFactory.PORT_DEFAULT));		
-		} catch (IOException e) {	 
+			//cache = new MemcachedClient(new InetSocketAddress("localhost", CacheFactory.PORT_DEFAULT));
+			cache = new MemcachedClient(new InetSocketAddress("ec2-50-112-96-199.us-west-2.compute.amazonaws.com", CacheFactory.PORT_DEFAULT));			
+		} catch (IOException e) {
 		   log.severe("memcache io exception");
-		   NnLogUtil.logException(e);
+		   cache = null;
 		} catch (Exception e) {
-			log.severe("memcache exception");
-			NnLogUtil.logException(e);
-		}
-		
+		   log.severe("memcache exception");
+		   cache = null;
+		}		
 		return cache;
+	}	
+
+	public static Object get(String key) {		
+		MemcachedClient cache = CacheFactory.getClient();
+		CacheFactory.isRunning = false;
+		Object obj = null;
+		try {
+			obj = cache.get(key);
+			CacheFactory.isRunning = true;
+		} catch (OperationTimeoutException e) {
+			log.severe("get OperationTimeoutException");
+		} catch (Exception e) {
+			log.severe("get Exception");
+			e.printStackTrace();
+		} finally {
+			cache.shutdown();			
+		}
+		return obj;
+	}	
+
+	public static Object set(String key, Object obj) {		
+		MemcachedClient cache = CacheFactory.getClient();
+		CacheFactory.isRunning = false;
+		Object myObj = null;
+		try {
+			cache.set(key, CacheFactory.EXP_DEFAULT, obj);
+			myObj = cache.get(key);
+			CacheFactory.isRunning = true;
+		} catch (OperationTimeoutException e) {
+			log.severe("set OperationTimeoutException");
+		} catch (Exception e) {
+			log.severe("set Exception");
+			e.printStackTrace();
+		} finally {
+			cache.shutdown();
+		}
+		return myObj;
+	}	
+
+	public static Object delete(String key) {		
+		MemcachedClient cache = CacheFactory.getClient();
+		CacheFactory.isRunning = false;
+		Object obj = null;
+		try {
+			cache.delete(key).get();
+			CacheFactory.isRunning = true;
+		} catch (OperationTimeoutException e) {
+			log.severe("get OperationTimeoutException");
+		} catch (Exception e) {
+			log.severe("get Exception");
+			e.printStackTrace();
+		} finally {
+			cache.shutdown();			
+		}
+		return obj;
 	}	
 	
 }
