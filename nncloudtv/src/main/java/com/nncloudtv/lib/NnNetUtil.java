31030e1dd2eb2fcb6c1861aeda9ce5b99513da4e
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-04-27 01:39:00 +0000

    look up ip with x-forwarded-for
    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso-moveout@2665 a0a5f996-2bb4-401c-8778-395c3c01700f

--- nncloudtv/src/main/java/com/nncloudtv/lib/NnNetUtil.java
+++ nncloudtv/src/main/java/com/nncloudtv/lib/NnNetUtil.java
@@ -9,6 +9,7 @@ import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang.text.StrTokenizer;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -42,6 +43,44 @@ public class NnNetUtil {
 		return host;
 	}
 
+	public static String getIp(HttpServletRequest req) {
+        String ip;
+        boolean found = false;
+        if ((ip = req.getHeader("x-forwarded-for")) != null) {
+          StrTokenizer tokenizer = new StrTokenizer(ip, ",");
+          while (tokenizer.hasNext()) {
+            ip = tokenizer.nextToken().trim();
+            if (isIPv4Valid(ip) && !isIPv4Private(ip)) {
+              found = true;
+              break;
+            }
+          }
+        }
+        if (!found) {
+          ip = req.getRemoteAddr();
+        }
+        return ip;		
+	}
+	
+    public static boolean isIPv4Private(String ip) {
+        long longIp = ipV4ToLong(ip);
+        return (longIp >= ipV4ToLong("10.0.0.0") && longIp <= ipV4ToLong("10.255.255.255")) ||
+            (longIp >= ipV4ToLong("172.16.0.0") && longIp <= ipV4ToLong("172.31.255.255")) ||
+            longIp >= ipV4ToLong("192.168.0.0") && longIp <= ipV4ToLong("192.168.255.255");
+      }    
+    
+    public static long ipV4ToLong(String ip) {
+        String[] octets = ip.split("\\.");
+        return (Long.parseLong(octets[0]) << 24) + (Integer.parseInt(octets[1]) << 16) +
+            (Integer.parseInt(octets[2]) << 8) + Integer.parseInt(octets[3]);
+    }    
+    
+    public static boolean isIPv4Valid(String ip) {
+    	String _255 = "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+    	Pattern pattern = Pattern.compile("^(?:" + _255 + "\\.){3}" + _255 + "$");
+        return pattern.matcher(ip).matches();
+    }
+	
 	public static void urlPostWithJson(String urlStr, Object obj) {
         URL url;
 		try {
