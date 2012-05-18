be764e4cc92a1cb32ad8f3eadbac23fd95d67be5
william <william@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-05-18 01:34:21 +0000

    change version number
    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso-moveout@2738 a0a5f996-2bb4-401c-8778-395c3c01700f

--- nncloudtv/src/main/java/com/nncloudtv/web/VersionController.java
+++ nncloudtv/src/main/java/com/nncloudtv/web/VersionController.java
@@ -12,9 +12,9 @@ public class VersionController {
 		
 	@RequestMapping("current")
 	public ResponseEntity<String> current() {
-        String appVersion = "0.0.9";
-        String svn = "2697";
-        String packagedTime = "2012-05-08 18:34:42.183000";
+        String appVersion = "3.1.11.2";
+        String svn = "2737";
+        String packagedTime = "2012-05-17 16:20:00.183000";
 		String info = "app version: " + appVersion + "\n"; 
 		info = info + "svn: " + svn + "\n";
 		info = info + "packaged time: " + packagedTime + "\n";
