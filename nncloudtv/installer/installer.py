b175fefaf9aa68d761a708821e56b8a49ef678e2
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-06-01 01:21:56 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso-moveout@2778 a0a5f996-2bb4-401c-8778-395c3c01700f

--- nncloudtv/installer/installer.py
+++ nncloudtv/installer/installer.py
@@ -9,7 +9,9 @@ from datetime import datetime
 #   4.upload files to server:
 #     prerequisites: your own key files, pscp(change to your own scp programs)
 
-myfile = open("..\\src\\main\\java\\com\\nncloudtv\\web\\VersionController.java", "rU")
+myos = raw_input('OS (1.Ubuntu 2.Windows) : ')
+
+myfile = open("..//src//main//java//com//nncloudtv//web//VersionController.java", "rU")
 version = ""
 for line in myfile:
   if (line.find("String appVersion") > 0):
@@ -23,17 +25,20 @@ if version == "":
 print "version number:" + version
 
 # copy over root.war
-src = "..\\target\\nncloudtv-0.0.1-SNAPSHOT.war"
+src = "..//target//nncloudtv-0.0.1-SNAPSHOT.war"
 dst = "root.war"
-shutil.copyfile(src, dst)
+shutil.copyfile(src, dst)                                                                                       
 print "--- generate root.war ---"
 
 # generate md5
-md5 = os.popen("tools\md5sums -u root.war").read()
+if myos == "1":
+  md5 = os.popen("md5sum root.war").read()
+if myos == "2":
+  md5 = os.popen("tools\md5sums -u root.war").read()
 match = re.match("(.*)( .*)", md5)
 if match:
-	 md5 = match.group(1)
-	 print "--- generate md5 = " + md5
+  md5 = match.group(1)
+print "--- generate md5 = " + md5
 
 dest = open("root.md5", "w")
 line = md5 + "  " + "root.war\x00\x0a"
@@ -48,25 +53,47 @@ dest.close()
 print "--- generate version file ---"
 
 # upload to server
-print "\n"
-server = raw_input('Environment (1.devel 2.alpha 3.stage 4.deploy 5.exit) : ')
+print "\n"                                             
+server = raw_input('Server (1.devel1 2.alpha 3.stage 4.deploy 5.exit) : ')
 if server != "5":
   print "--- " + datetime.now().strftime("%d/%m/%y %H:%M:%S %Z") + "---"
 if server == "1":
-  print "--- uploading to devel server ---" 
-  os.system("pscp -i dev-west2.ppk root.war ubuntu@ec2-50-112-54-59.us-west-2.compute.amazonaws.com:/home/ubuntu/files")
+  print "--- uploading to devel1 server ---"
+  if myos == "1":
+    os.system("scp -i ~/keys/dev-west2.pem root.war ubuntu@ec2-50-112-54-59.us-west-2.compute.amazonaws.com:/home/ubuntu/files/root.war")
+  if myos == "2":
+    os.system("pscp -i dev-west2.ppk root.war ubuntu@ec2-50-112-54-59.us-west-2.compute.amazonaws.com:/home/ubuntu/files")
 if server == "2":
   print "--- uploading to alpha server ---"
-  os.system("pscp -i dev-west2.ppk root.war ubuntu@alpha_log.9x9.tv:/var/www/updates/" + version)
+  folder = raw_input('Folder created? : ')  # to be hooked up with ssh command, used as reminder for now
+  if myos == "1":            
+    os.system("scp -i ~/keys/dev-west2.pem root.war ubuntu@alpha_log.9x9.tv:/var/www/updates/" + version + "/root.war")
+    os.system("scp -i ~/keys/dev-west2.pem root.md5 ubuntu@alpha_log.9x9.tv:/var/www/updates/" + version + "/root.md5")
+    os.system("scp -i ~/keys/dev-west2.pem version ubuntu@alpha_log.9x9.tv:/var/www/updates/version")  
+    os.system("scp -i ~/keys/dev-west2.pem version ubuntu@alpha_log.9x9.tv:/var/www/updates/" + version + "/version")
+  if myos == "2":           
+     os.system("pscp -i dev-west2.ppk root.war ubuntu@alpha_log.9x9.tv:/var/www/updates/" + version)
+     os.system("pscp -i dev-west2.ppk root.md5 ubuntu@alpha_log.9x9.tv:/var/www/updates/" + version)
+     os.system("pscp -i dev-west2.ppk version ubuntu@alpha_log.9x9.tv:/var/www/updates")  
+     os.system("pscp -i dev-west2.ppk version ubuntu@alpha_log.9x9.tv:/var/www/updates/" + version)
 if server == "3":
   print "--- uploading to stage server ---"
-  os.system("pscp -i prod-west2.ppk root.war ubuntu@ec2-50-112-111-245.us-west-2.compute.amazonaws.com:/home/ubuntu/files")
+  if myos == "1":             
+    os.system("scp -i ~/keys/prod-west2.ppk root.war ubuntu@ec2-50-112-111-245.us-west-2.compute.amazonaws.com:/home/ubuntu/files/root.war")
+  if myos == "2": 
+    os.system("pscp -i prod-west2.ppk root.war ubuntu@ec2-50-112-111-245.us-west-2.compute.amazonaws.com:/home/ubuntu/files")
 if server == "4":
   print "--- uploading to deploy server ---"
   folder = raw_input('Folder created? : ')  # to be hooked up with ssh command, used as reminder for now
-  os.system("pscp -i prod-west2.ppk root.war ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/" + version)
-  os.system("pscp -i prod-west2.ppk root.md5 ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/" + version)
-  os.system("pscp -i prod-west2.ppk version ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates")  
-  os.system("pscp -i prod-west2.ppk version ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/" + version)
+  if myos == "1":                  
+     os.system("scp -i ~/keys/prod-west2.pem root.war ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/" + version + "/root.war")
+     os.system("scp -i ~/keys/prod-west2.pem root.md5 ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/" + version + "/root.md5")
+     os.system("scp -i ~/keys/prod-west2.pem version ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/version")  
+     os.system("scp -i ~/keys/prod-west2.pem version ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/" + version + "/version")
+  if myos == "2": 
+     os.system("pscp -i prod-west2.ppk root.war ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/" + version)
+     os.system("pscp -i prod-west2.ppk root.md5 ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/" + version)
+     os.system("pscp -i prod-west2.ppk version ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates")  
+     os.system("pscp -i prod-west2.ppk version ubuntu@ec2-50-112-96-199.us-west-2.compute.amazonaws.com:/var/www/updates/" + version)
 
 print "--- " + datetime.now().strftime("%d/%m/%y %H:%M:%S %Z") + "---"  
