42ad2fff41144543290587db404f043f5a352947
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-06-05 01:31:07 +0000

    git-svn-id: https://everest.teltel.com:8443/repos/misc/web/nnvmso-moveout@2798 a0a5f996-2bb4-401c-8778-395c3c01700f

--- nncloudtv/installer/s3cmd.py
+++ nncloudtv/installer/s3cmd.py
@@ -1,5 +1,7 @@
 import os
 
-os.system("s3cmd sync --exclude '**.svn**' ../src/main/webapp/images/ s3://9x9ui/war/v0/images/")
-os.system("s3cmd sync --exclude '**.svn**' ../src/main/webapp/javascripts/ s3://9x9ui/war/v0/javascripts/")
-os.system("s3cmd sync --exclude '**.svn**' ../src/main/webapp/stylesheets/ s3://9x9ui/war/v0/stylesheets/")
+os.system("s3cmd sync —acl-public --exclude '**.svn**' ../src/main/webapp/images/ s3://9x9ui/war/v0/images/")
+os.system("s3cmd sync —acl-public --exclude '**.svn**' ../src/main/webapp/javascripts/ s3://9x9ui/war/v0/javascripts/")
+os.system("s3cmd sync —acl-public --exclude '**.svn**' ../src/main/webapp/stylesheets/ s3://9x9ui/war/v0/stylesheets/")
+
+# s3cmd setacl --acl-public --recursive s3://9x9ui/war/v0/
